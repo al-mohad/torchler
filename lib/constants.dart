276e5eab2d806cfce1f5361de973623cb1326722
Musa <musjib999@gmail.com>
2020-05-10 15:31:58 +0100

    second commit

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kOffButtonTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF999999),
+);
+
+const kOnButtonTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
--- lib/torch_light.dart
+++ lib/torch_light.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:torch_light/constants.dart';
 
 class TorchLight extends StatefulWidget {
   @override
@@ -10,12 +11,61 @@ class _TorchLightState extends State<TorchLight> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Icon(
-            FontAwesomeIcons.lightbulb,
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Center(
+                child: Icon(
+                  FontAwesomeIcons.lightbulb,
+                  size: 200.0,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(40.0, 50.0, 0, 0),
+                    padding: EdgeInsets.all(20.0),
+                    width: 100.0,
+                    height: 80.0,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2F2F2),
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), bottomLeft: Radius.circular(20.0), ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'OFF',
+                        style: kOffButtonTextStyle,
+                      ),
+                    ),
+                  ),
+                ),
+                 Expanded(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 50.0, 40.0, 0),
+                    padding: EdgeInsets.all(20.0),
+                    width: 100.0,
+                    height: 80.0,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFEA200).withOpacity(0.5),
+                      borderRadius: BorderRadius.only(topRight: Radius.circular(20.0), bottomRight: Radius.circular(20.0)),
+                    ),                    
+                    child: Center(
+                      child: Text(
+                        'ON',
+                        style: kOnButtonTextStyle,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
