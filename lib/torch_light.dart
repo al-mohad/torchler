d1b3019bab2a5fe5fdfb1844d70ebcf48010c770
Muhammad Buhari <gmbdairy@gmail.com>
2020-05-11 19:03:33 +0100

    Added functionality of light on and light off

--- lib/torch_light.dart
+++ lib/torch_light.dart
@@ -1,3 +1,4 @@
+import 'package:flashlight/flashlight.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:torch_light/constants.dart';
@@ -8,6 +9,20 @@ class TorchLight extends StatefulWidget {
 }
 
 class _TorchLightState extends State<TorchLight> {
+  bool _hasFlashlight = false;
+  initFlashlight() async {
+    bool hasFlashlight = await Flashlight.hasFlashlight;
+    setState(() {
+      _hasFlashlight = hasFlashlight;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initFlashlight();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,17 +49,23 @@ class _TorchLightState extends State<TorchLight> {
                     height: 80.0,
                     decoration: BoxDecoration(
                       color: Color(0xFFF2F2F2),
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), bottomLeft: Radius.circular(20.0), ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        bottomLeft: Radius.circular(20.0),
+                      ),
                     ),
                     child: Center(
-                      child: Text(
-                        'OFF',
-                        style: kOffButtonTextStyle,
+                      child: GestureDetector(
+                        onTap: () => Flashlight.lightOff(),
+                        child: Text(
+                          'OFF',
+                          style: kOffButtonTextStyle,
+                        ),
                       ),
                     ),
                   ),
                 ),
-                 Expanded(
+                Expanded(
                   child: Container(
                     margin: EdgeInsets.fromLTRB(0, 50.0, 40.0, 0),
                     padding: EdgeInsets.all(20.0),
@@ -52,12 +73,17 @@ class _TorchLightState extends State<TorchLight> {
                     height: 80.0,
                     decoration: BoxDecoration(
                       color: Color(0xFFFEA200).withOpacity(0.5),
-                      borderRadius: BorderRadius.only(topRight: Radius.circular(20.0), bottomRight: Radius.circular(20.0)),
-                    ),                    
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0)),
+                    ),
                     child: Center(
-                      child: Text(
-                        'ON',
-                        style: kOnButtonTextStyle,
+                      child: GestureDetector(
+                        onTap: () => Flashlight.lightOn(),
+                        child: Text(
+                          'ON',
+                          style: kOnButtonTextStyle,
+                        ),
                       ),
                     ),
                   ),
